22d4a35d589acd697465369f0990350e465128d2
Mark De May <markd@frii.com>
2017-12-27 10:48:24 -0700

    Update cors.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,23 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins 'https://beer-mavin.herokuapp.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
     origins 'localhost:3001'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'localhost:5000'
 
     resource '*',
       headers: :any,
